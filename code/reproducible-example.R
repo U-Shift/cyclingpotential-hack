36289c76dfb2fe07fdf4936d8b36ca26878cd3ff
Robin Lovelace <rob00x@gmail.com>
2020-09-15 17:01:07 +0100

    Update repro example

--- code/reproducible-example.R
+++ code/reproducible-example.R
@@ -3,12 +3,6 @@
 install.packages("remotes", quiet = TRUE)
 remotes::install_cran(c("sf", "stplanr", "pct", "tmap", "dplyr"), quiet = TRUE)
 
-# piggyback::pb_upload("od_data_final.csv")
-# piggyback::pb_upload("desire_lines_final.geojson")
-# piggyback::pb_upload("districts.geojson")
-# # piggyback::pb_download(file = "CENTROIDS.Rds", repo = "U-Shift/pct-lisbon2")
-# # centroids = readRDS("CENTROIDS.Rds")
-# st_write(centroids, "centroids.geojson")
 u_od = "od_data_final.csv"
 od_data = read.csv("od_data_final.csv")
 head(od_data)
@@ -16,39 +10,38 @@ plot(od_data$Length_euclidean, od_data$pcycle_current)
 
 library(sf)
 # test the sf package
-# u1 = "https://github.com/U-Shift/cyclingpotential-hack/releases/download/1.0/city_centroids.geojson"
-u1 = "districts.geojson"
-u1b = "centroids.geojson"
-districts = read_sf(u1)
-plot(centroids)
-centroids_geo = st_centroid(districts)
+u1 = "https://github.com/U-Shift/cyclingpotential-hack/releases/download/2.0.0/centroids.geojson"
+u1b = "https://github.com/U-Shift/cyclingpotential-hack/releases/download/2.0.0/districts.geojson"
+districts = read_sf(u1b)
+centroids = read_sf(u1)
 plot(districts$geometry)
+centroids_geo = st_centroid(districts)
 plot(centroids$geometry, add = TRUE)
 plot(centroids_geo$geometry, add = TRUE, col = "red")
 
 # check interactive mapping with tmap
 library(tmap)
 tmap_mode("view")
-# u2 = "https://github.com/U-Shift/cyclingpotential-hack/releases/download/1.0/desire_lines_integers.geojson"
-u2 = "desire_lines_final.geojson"
+u2 = "https://github.com/U-Shift/cyclingpotential-hack/releases/download/2.0.0/desire_lines_final.geojson"
 desire_lines = sf::read_sf(u2)
 tm_shape(desire_lines) +
   tm_lines(col = "Bike", palette = "viridis", lwd = "Car", scale = 9)
 
 # check route network generation with stplanr
 library(stplanr)
-# u3 = "https://github.com/U-Shift/cyclingpotential-hack/releases/download/1.0/routes_integers_cs_balanced.geojson"
-u3 = "routes_fast.geojson"
+u3 = "https://github.com/U-Shift/cyclingpotential-hack/releases/download/2.0.0/routes_fast.geojson"
 routes = sf::read_sf(u3)
-tm_shape(routes[]) +
-  tm_lines()
+library(dplyr)
+routes_1 = routes %>% 
+  filter(route_number == 1)
+tm_shape(routes_1) +
+  tm_lines("gradient_segment", palette = "viridis")
 rnet = overline(routes, "Bike") 
 tm_shape(rnet) +
   tm_lines(scale = 5, col = "Bike", palette = "viridis")
 
 # check analysis with dplyr and estimation of cycling uptake with pct function
 library(pct)
-library(dplyr)
 route_segments_balanced = sf::read_sf(u3)
 routes_balanced = route_segments_balanced %>% 
   group_by(DICOFREor11, DICOFREde11) %>% 
