3a609065c4f058410babd5587531838191a6a82a
RFlx <39107166+temospena@users.noreply.github.com>
2020-09-25 16:29:31 +0100

    Add files via upload
    https://github.com/U-Shift/cyclingpotential-hack/issues/8

---
+++ minimumPCTrequiremens.R
@@ -0,0 +1,35 @@
+#Minimum requirements for a PCT
+
+## Zones
+Divide the city into zones
+Census
+  Where people live and work
+Divide in our own zones
+  Package zonebiulder [url()]
+  
+  ### which zoom level?
+  As long as you can walk or cycle between 2 zones?
+  random point for O and D in the same zone?
+    what about if 50% trips are made in the same zone
+  
+  #And use always the lower level, or aggregate?
+    not too detailed
+    computation intensively
+
+  
+### OD data at those zones level
+  cycling
+  car
+  
+## Route options
+  Journey planners, such as cycliestreets algorithm
+  ROuting services - route_fun = cyclestreets::journey (see other route_fun avaliable)
+  
+  
+    
+# Road network
+  From OSM maps? even with poor quality
+
+  
+# Use PCT with our own spatial road network?
+# # and with our own cycling route functions?
